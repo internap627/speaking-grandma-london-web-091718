1d21bd523a1506211340f2c176e3da98fa07ebf1
Nkosi Phillip <internap627@gmail.com>
2018-09-12 22:50:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,10 @@
 def speak_to_grandma(str)
  if str == 'I LOVE YOU GRANDMA!'
   puts 'I LOVE YOU TOO PUMPKIN!'
-  elsif 
+  elsif str == str.upcase
+    puts 'NO, NOT SINCE 1938!'
+  else
+    puts 
   
 end
 end
